aa4dd756c6f85c0dc76f0fad942558b9299ce2bd
Emily Langworth <e24murph@gmail.com>
2023-02-27 11:55:26 -0800

    added target blank to links in footer nav

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-function Footer() {
+function Footer(props) {
+  const tabs = ["LinkedIn", "StackOverFlow", "Github"];
   return (
     <footer>
       <nav className="tabs is-centered">
         <ul className="nav nav-tabs">
-          <li className="nav-item tabs-link-color"><a className="my-color" href="https://www.linkedin.com/in/emilylangworth/">LinkedIn</a></li>
-          <li className="nav-item tabs-link-color"><a href="https://stackoverflow.com/users/20472615/emily-langworth">Stack OverFlow</a></li>
-          <li className="nav-item tabs-link-color"><a href="https://github.com/elangworth">Github</a></li>
+          <li className="nav-item tabs-link-color"><a className="my-color" href="https://www.linkedin.com/in/emilylangworth/" target="_blank">LinkedIn</a></li>
+          <li className="nav-item tabs-link-color"><a className="my-color" href="https://stackoverflow.com/users/20472615/emily-langworth" target="_blank">Stack OverFlow</a></li>
+          <li className="nav-item tabs-link-color"><a className="my-color" href="https://github.com/elangworth"target="_blank">Github</a></li>
         </ul>
       </nav>
     </footer>
