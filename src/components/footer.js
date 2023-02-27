48329b9fb9f4295f84f4828a76627a707253eae3
Emily Langworth <e24murph@gmail.com>
2023-02-27 11:20:38 -0800

    moved css style to the correct file to fix fotter to the bottom and make the body 100% height

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <footer>
       <nav className="tabs is-centered">
         <ul className="nav nav-tabs">
-          <li className="nav-item tabs-link-color"><a className="tabs-link-color" href="https://www.linkedin.com/in/emilylangworth/">LinkedIn</a></li>
+          <li className="nav-item tabs-link-color"><a className="my-color" href="https://www.linkedin.com/in/emilylangworth/">LinkedIn</a></li>
           <li className="nav-item tabs-link-color"><a href="https://stackoverflow.com/users/20472615/emily-langworth">Stack OverFlow</a></li>
           <li className="nav-item tabs-link-color"><a href="https://github.com/elangworth">Github</a></li>
         </ul>
